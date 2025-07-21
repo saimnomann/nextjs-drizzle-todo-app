2e6ecd23f10fed3b4044ef3e3152dd5c25d23702
saimnomann <muhammadsaimnoman24@gmail.com>
2025-07-22 02:36:49 +0500

    Bugs Fixed

--- src/app/(components)/editTask/index.tsx
+++ src/app/(components)/editTask/index.tsx
@@ -40,8 +40,8 @@ finally{
 return(
 
         <div>
-            {allow?<form onSubmit={handleEdit} className="flex gap-2  rounded-full px-4 py-2">
-            <input type="text" value={editTask} onChange={(e)=>seteditTask(e.target.value)} className=" w-20 md:w-44 p-2 rounded-2xl text-sm shadow-md outline-none bg-gray-300"></input>
+            {allow?<form onSubmit={handleEdit} className="flex gap-2  rounded-full px-4 py-2 w-40 md:w-40 md:px-35">
+            <input type="text" value={editTask} onChange={(e)=>seteditTask(e.target.value)} className=" w-15 md:w:60 p-2 rounded-2xl text-sm shadow-md outline-none bg-gray-300"></input>
            <button type="submit" className=" bg-blue-600 text-sm  text-white px-3 py-2 hover:cursor-pointer hover:scale-105 rounded-full">Save</button>
             </form>:
             <button onClick={()=>{setallow(true)}}><Image src="/edit.jpg" alt="Edit" width={20} height={20} className="hover:cursor-pointer"/>
